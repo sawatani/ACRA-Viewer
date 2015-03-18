cacd049335be6f8c2dbd075940ff0264d7ebc736
Kunio Sawatani <devel@fathens.org>
2015-03-18 15:22:26 +0900

    Cache list of applications

--- lib/component/apps-list.dart
+++ lib/component/apps-list.dart
@@ -1,7 +1,5 @@
 library apps_list_component;
 
-import 'dart:html';
-
 import 'package:angular/angular.dart';
 import 'package:acra_viewer/service/dynamodb.dart';
 
@@ -13,22 +11,9 @@ class AppsListComponent {
   DynamoDB db;
 
   String message = "Loading list ...";
-  List<App> allApps;
+  List<App> get allApps => db.allApps;
 
   AppsListComponent(this.db, RouteProvider routeProvider) {
     _appId = routeProvider.parameters['appId'];
-    obtainApps();
-    window.addEventListener("AWS.RENEW_CREDENTIAL", (Event event) {
-      obtainApps();
-    });
-  }
-
-  void obtainApps() {
-    print("Start to obtain applications list");
-    db.allApps.then((List<App> list) {
-      allApps = list;
-    }).catchError((error) {
-      print("Failed to get applications list: ${error}");
-    });
   }
 }
--- lib/service/dynamodb.dart
+++ lib/service/dynamodb.dart
@@ -3,41 +3,58 @@ library dynamodb;
 import 'dart:async';
 import 'dart:convert';
 import 'dart:js';
+import 'dart:html';
 
 import 'package:angular/angular.dart';
 
 @Injectable()
 class DynamoDB {
-  Future<List<App>> get allApps => loadAllApps();
-
-  Future<List<App>> loadAllApps() => new _DB().allList('APPLICATIONS', (item) {
+  final _apps = new Delegate<App>("APPLICATIONS", (JsObject item) {
     final id = item['ID']['S'];
     final tableName = item['CONFIG']['M']['tableName']['S'];
     return new App(id, tableName);
   });
 
-  Future<List<Report>> loadAllReports(String tableName) => new _DB().allList(tableName, (item) {
-    final id = item['ID']['S'];
-    final created = item['CREATED_AT']['S'];
-    final text = item['REPORT']['S'];
-    return new Report(id, DateTime.parse(created), text);
-  });
+  List<App> _cachedApps = null;
+  List<App> get allApps => _cachedApps;
+
+  DynamoDB() {
+    window.addEventListener("AWS.RENEW_CREDENTIAL", (Event event) {
+      refreshApps();
+    });
+  }
+
+  void refreshApps() {
+    print("Start to refresh applications list");
+    _apps.allList().then((List<App> list) {
+      _cachedApps = list;
+    }).catchError((error) {
+      _cachedApps = null;
+      print("Failed to get applications list: ${error}");
+    });
+  }
 }
 
-class _DB {
-  final db = new JsObject(context['AWS']['DynamoDB']);
+class Delegate<T> {
+  static final db = new JsObject(context['AWS']['DynamoDB']);
 
-  Future<List> allList(String tableName, f(JsObject item)) => scan({
-    'TableName': "ACRA.${tableName}"
-  }, f);
+  final String tableName;
+  var _converter;
 
-  Future<List> scan(Map<String, String> params, f(JsObject item)) {
+  Delegate(this.tableName, T f(JsObject item)) {
+    _converter = f;
+  }
+
+  Future<List<T>> allList() => scan({});
+
+  Future<List<T>> scan(Map<String, String> params) {
+    params['TableName'] = "ACRA.${tableName}";
     final result = new Completer();
     db.callMethod('scan', [new JsObject.jsify(params), (error, JsObject data) {
         if (error != null) {
           result.completeError(error);
         } else {
-          result.complete(data['Items'].map(f).toList());
+          result.complete(data['Items'].map(_converter).toList());
         }
       }]);
     print("Returning temporaly future...");
@@ -48,10 +65,19 @@ class _DB {
 class App {
   final String id;
   final String tableName;
+  var _table;
 
   App(this.id, this.tableName) {
     print("Created app: ${id} with ${tableName}");
+    _table = new Delegate<Report>(tableName, (item) {
+      final id = item['ID']['S'];
+      final created = item['CREATED_AT']['S'];
+      final text = item['REPORT']['S'];
+      return new Report(id, DateTime.parse(created), text);
+    });
   }
+
+  Future<List<Report>> allReports() => _table.allList();
 }
 
 class Report {
